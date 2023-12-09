f227c7b748fa2b43d415b4a5e59c34a49ea0234e
league <league@imac-22.attlocal.net>
2023-12-08 16:56:46 -0800

    houses

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,5 +1,7 @@
 package _01_methods._1_houses;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 /*
@@ -18,6 +20,30 @@ public class Houses {
 		rob.penDown();
 		rob.setPenColor(0, 0, 255);
 		rob.setSpeed(20);
+		rob.moveTo(20, 500);
+		//drawHouse(200);
+		//drawHouse(150);
+		//drawHouse(100);
+		//drawHouse(250);
+		//drawHouse(175);
+		//drawHouse(75);
+		//drawHouse(200);
+		//drawHouse(115);
+		//drawHouse(50);
+		//drawHouse("small");
+		//drawHouse("medium");
+	//	drawHouse("large");
+		drawHouse("small",Color.blue);
+		drawHouse("medium",new Color(16,104,112));
+		drawHouse("large",new Color(134, 84, 179));
+		drawHouse("medium",new Color(157, 196, 190));
+		drawHouse("large",new Color(194, 199, 153));
+		drawHouse("small",new Color(177, 204, 155));
+		drawHouse("large",new Color(113, 73, 156));
+		drawHouse("medium",new Color(166, 85, 126));
+		drawHouse("large",new Color(22, 145, 186));
+		
+		
 		//		rob.moveTo(20, 600);
 		//		rob.move(50);
 		//		rob.turn(90);
@@ -34,23 +60,35 @@ public class Houses {
 
 	}
 	
-	void drawHouse(int height) {
-	rob.moveTo(20, 600);
-	rob.move(50);
-	rob.turn(90);
-	rob.move(100);
-	rob.turn(280);
-	rob.move(200);
+	void drawHouse(int height,Color color) {
+	rob.setPenColor(color);
+	rob.move(height);
 	rob.turn(90);
 	rob.move(50);
 	rob.turn(90);
-	rob.move(200);
-	rob.setPenColor(0,255,0);
-	rob.turn(90);
-	rob.move(50);
+	rob.move(height);
+	rob.turn(-90);
+	rob.setPenColor(0, 255, 0);
+	rob.move(45);
+	rob.turn(-90);
+rob.setPenColor(0,0,255);
 	}
 	
+	void drawHouse(String height,Color color) {
+		
+		if(height.equals("small")) {
+			drawHouse(60,color);
+			}
+		if(height.equals("medium")) {
+			drawHouse(120,color);
+		}
+		if(height.equals("large")) {
+			drawHouse(250,color);
+			
+		}
+	}
 }
+
 
 
 
